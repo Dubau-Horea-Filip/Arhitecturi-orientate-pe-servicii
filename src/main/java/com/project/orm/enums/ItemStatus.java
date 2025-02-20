6731b67ce92fd258ccc7d28dae862b86f269103d
Dubau-Horea-Filip <filipdubau@yahoo.ro>
2025-02-20 13:08:15 +0200

    tutorial

--- src/main/java/com/project/orm/enums/ItemStatus.java
+++
@@ -1,6 +0,0 @@
-package com.project.orm.enums;
-
-public enum ItemStatus {
-    SOLD,
-    AVAILABLE;
-}
--- src/main/java/com/project/orm/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.orm.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.project.orm.model.Item;
-
-@Repository
-public interface ItemRepository extends JpaRepository<Item, Integer> {
-}
